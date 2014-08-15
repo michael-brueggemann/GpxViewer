f0416d7f5b41045854daf0bfe18f49b64bd08947
Michael Brueggemann <michael.brueggemann@metafinanz.de>
2014-08-15 22:37:12 +0200

    * tourlist filters added

--- app/js/directives/tourlistDir.js
+++ app/js/directives/tourlistDir.js
@@ -11,52 +11,101 @@ angular.module('gpxViewer').controller('tourlistDirCtrl', ['$scope', 'gpxParser'
 		var log = log4javascript.getLogger("gpxViewer.directives.tourlistDirCtrl");
 		log.debug('creating directive controller: tourlistDirCtrl');
 
-		$scope.search = {};
-		$scope.search.name = '';
+		$scope.filter = {};
+		$scope.filter.name = '';
 
 		$scope.tourlistMetaData = new Array();
-
-		$scope.selectionChanged = function(event, tour) {
-			log.debug('selectionChanged', tour.name);
-
-			var checkbox = event.target;
-//			if (checkbox.checked) {
-//				tour.color = colorService.getColor(tour.path);
-//			} else {
-//				tour.color = 'black';
-//				colorService.removeColor(tour.path);
-//			}
-		};
 
 		$scope.selectionChangedForAll = function(event) {
 			log.debug('selectionChangedForAll');
 
 			var checkbox = event.target;
-			
-			for(var id in $scope.tourlist) {
-				$scope.tourlist[id].selected = checkbox.checked;
+
+			for (var id in $scope.tourlistMetaData) {
+				var tourMetaData = $scope.tourlistMetaData[id];
+				// only change selected state for entries not filtered out
+				if (tourMetaData.isFilterVisible === true) {
+//					$scope.tourlist[tourMetaData.id].selected = checkbox.checked;
+					tourMetaData.selected = checkbox.checked;
+				}
 			}
 		};
 
 		$scope.myFilter = function(criteria) {
+			log.debug('myFilter()');
+
 			return function(tour) {
-				var result = false;
+				log.trace('check filter for id=', tour.id);
+				var result = true;
+
+				var filterName = $scope.filter.name || '';
+				var filterDistance = $scope.filter.distance || '';
+				var filterEleRise = $scope.filter.eleRise || '';
+
 				// don't filter if filter string is empty
-				if ($scope.search.name.length == 0) {
-					result = true;
-				}
 				// ok when name matches
-				else if (tour.name.indexOf($scope.search.name) > -1) {
-					result = true;
-//					tour.selected = true;
+				if (filterName.length === 0
+						|| tour.name.toLowerCase().indexOf(filterName.toLowerCase()) > -1) {
+					log.debug('name filter: visible=true');
+					result = result && true;
 				}
 				// not in result => set selected to false
 				else {
+					log.debug('name filter: visible=false');
 					tour.selected = false;
+					result = result && false;
 				}
+
+				result = result && filterByNumberString(filterDistance, tour.stats.distance, tour);
+				result = result && filterByNumberString(filterEleRise, tour.stats.eleRise, tour);
+
+				tour.isFilterVisible = result;
 				return result;
 			};
 		};
+
+		/**
+		 * 
+		 * @param {String} filterString e.g. "<500"
+		 * @param {Number} tourValue e.g. 465
+		 * @param {Tour} tour
+		 * @returns {Boolean}
+		 */
+		function filterByNumberString(filterString, tourValue, tour) {
+
+			// don't filter if filter string is empty
+			if (filterString.length <= 1) {
+				log.debug('filter: "empty" => visible=true');
+				return true;
+			} else {
+				log.debug('filter: visible=true');
+				var sign = filterString.substring(0, 1);
+				var value = filterString.substring(1);
+				if (sign === '>') {
+					log.debug('filter: >');
+					if (tourValue >= value) {
+						log.debug('   visible=true');
+						return true;
+					} else {
+						log.debug('   visible=false');
+						tour.selected = false;
+						return false;
+					}
+				} else if (sign === '<') {
+					log.debug('filter: <');
+					if (tourValue <= value) {
+						log.debug('   visible=true');
+						return true;
+					} else {
+						log.debug('   visible=false');
+						tour.selected = false;
+						return false;
+					}
+				} else {
+					log.info('wrong filter:', filterString);
+				}
+			}
+		}
 
 		$scope.showDesc = function(desc) {
 			//console.log(desc);
@@ -65,12 +114,13 @@ angular.module('gpxViewer').controller('tourlistDirCtrl', ['$scope', 'gpxParser'
 		$scope.$watch('tourlistMetaData', function(newTourlistMetaData, oldTourlistMetaData) {
 			log.trace('watch for tourlistMetaData triggered');
 			if (Object.keys(newTourlistMetaData).length !== Object.keys(oldTourlistMetaData).length) {
-				changed();
+//				changed();
 			} else {
 				for (var index in newTourlistMetaData) {
 					if (newTourlistMetaData[index].selected !== oldTourlistMetaData[index].selected) {
+						log.debug('sync selected state for id:', newTourlistMetaData[index].id);
 						$scope.tourlist[newTourlistMetaData[index].id].selected = newTourlistMetaData[index].selected;
-						return false;
+//						return false;
 					}
 				}
 			}
@@ -82,14 +132,15 @@ angular.module('gpxViewer').controller('tourlistDirCtrl', ['$scope', 'gpxParser'
 			log.trace('watch for tourlist triggered');
 			if (Object.keys(newTourlist).length !== Object.keys(oldTourlist).length) {
 				changed();
-			} else {
-				for (var id in newTourlist) {
-					if (newTourlist[id].selected !== oldTourlist[id].selected) {
-						changed();
-						return false;
-					}
-				}
 			}
+//			else {
+//				for (var id in newTourlist) {
+//					if (newTourlist[id].selected !== oldTourlist[id].selected) {
+//						changed();
+//						return false;
+//					}
+//				}
+//			}
 		},
 				true);
 
